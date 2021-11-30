0d55a1240e7425109e8d19e12fb1740315880bc0
unknown <juslepi@gmail.com>
2021-11-30 12:36:33 +0200

    text-color refactor

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,16 +10,12 @@ import { PlayIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="bg-gray-800 p-2 flex items-center">
-      <MenuIcon className="h-6 w-6 mx-4 text-white" />
+    <div className="text-white bg-gray-800 p-2 flex items-center">
+      <MenuIcon className="h-6 w-6 mx-4 " />
       <div className="flex items-center mx-8">
-        <PlayIcon className="h-6 w-10 text-white border-4 border-red-600 rounded-md bg-red-600" />
-        <p className="text-xl font-semibold text-white tracking-tighter">
-          YouTube
-        </p>
-        <p className="relative bottom-1 left-1 text-white text-xs font-thin b-2">
-          FI
-        </p>
+        <PlayIcon className="h-6 w-10  border-4 border-red-600 rounded-md bg-red-600" />
+        <p className="text-xl font-semibold  tracking-tighter">YouTube</p>
+        <p className="relative bottom-1 left-1  text-xs font-thin b-2">FI</p>
       </div>
       <form className="flex items-center flex-grow">
         <input
@@ -27,15 +23,15 @@ const Header = () => {
           type="text"
           placeholder="Search"
         />
-        <SearchIcon className="p-4 h-12 w-16 text-white md:bg-gray-700" />
-        <MicrophoneIcon className="p-2 h-10 w-10 ml-2 text-white bg-gray-900 rounded-full" />
+        <SearchIcon className="p-4 h-12 w-16  md:bg-gray-700" />
+        <MicrophoneIcon className="p-2 h-10 w-10 ml-2  bg-gray-900 rounded-full" />
       </form>
       {/* Right Side Icons */}
       <div className="flex mx-4 w-2/6 max-w-xs items-center justify-around">
-        <VideoCameraIcon className="h-6 w-6 text-white" />
-        <ViewGridIcon className="h-6 w-6 text-white" />
-        <BellIcon className="h-6 w-6 text-white" />
-        <button className="text-white font-bold px-4 py-2 bg-gray-400 rounded-full">
+        <VideoCameraIcon className="h-6 w-6 " />
+        <ViewGridIcon className="h-6 w-6 " />
+        <BellIcon className="h-6 w-6 " />
+        <button className=" font-bold px-4 py-2 bg-gray-400 rounded-full">
           J
         </button>
       </div>
