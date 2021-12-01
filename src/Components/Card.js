a317fb3d7a4228f0cb8d405e2d4dff4d93bef628
unknown <juslepi@gmail.com>
2021-12-01 18:46:47 +0200

    Api data

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,19 @@
-const Card = () => {
+const Card = ({ title, channel, views, thumbnail }) => {
   //Thumnail, uploader, views, upload-time
   return (
     <div className="card m-2 text-gray-400 text-xs cursor-pointer">
-      <div id="thumbnail" className="thumbnail mb-2">
+      {/* <div id="thumbnail" className="thumbnail mb-2">
         ?
-      </div>
+      </div> */}
       <div className="flex">
         <div className="avatar bg-gray-400 rounded-full">A</div>
+        <img src={thumbnail} className="thumbnail" alt="thumbnail" />
         <div className="ml-2">
           <p className="mb-2 text-white text-base font-semibold leading-4">
-            Very interesting video
+            {title}
           </p>
-          <p>Uploader</p>
-          <p>Views</p>
+          <p>{channel}</p>
+          <p>{views / 1000} t. katselukertaa</p>
           <p>2 kuukautta sitten</p>
         </div>
       </div>
--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 import Card from "./Card";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
+
   useEffect(() => {
-    const tmp = [];
-    for (let i = 0; i < 10; i++) {
-      tmp.push(i);
-    }
-    setVideos(tmp);
-  }, []);
+    axios
+      .get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((res) => setVideos(res.data.items));
+  });
   return (
     <div className="flex justify-center md:justify-start min-w-full h-full flex-wrap text-white bg-gray-900 m-2">
       {videos.map((video, index) => {
-        return <Card key={index} />;
+        return (
+          <Card
+            key={index}
+            title={video.snippet.title}
+            channel={video.snippet.channelTitle}
+            views={video.statistics.viewCount}
+            thumbnail={video.snippet.thumbnails.medium.url}
+            uploaded={video.snippet.publishedAt}
+          />
+        );
       })}
     </div>
   );
 };
 
 export default VideoContainer;
+
+// useEffect(() => {
+//   const tmp = [];
+//   for (let i = 0; i < 10; i++) {
+//     tmp.push(i);
+//   }
+//   setVideos(tmp);
+// }, []);
